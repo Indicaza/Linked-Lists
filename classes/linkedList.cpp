01920d15d36c2fa190be8232dc30569500a1613c
zachreitmeier@yahoo.com <indicazas@gmail.com>
2022-07-15 05:34:20 -0400

    Commit before splitting class

---
+++ classes/linkedList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zach Reitmeier on 7/15/22.
+//
+
+#include "linkedList.h"
---
+++ classes/linkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by Zach Reitmeier on 7/15/22.
+//
+
+#ifndef MP3_LINKEDLIST_H
+#define MP3_LINKEDLIST_H
+
+
+class linkedList {
+
+};
+
+
+#endif //MP3_LINKEDLIST_H
